c8c5d0f781a338b5a986431ba32649de4d7454fe
Vasily <florionpvp@gmail.com>
2018-11-21 23:17:53 +0300

    Версия для сервера

--- src/Deffi/Main.java
+++ src/Deffi/Main.java
@@ -1,70 +1,27 @@
 package Deffi;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.math.BigInteger;
 import java.util.Random;
-public class Main {
-    public static void main(String[] args){
-        int number;
-        int n = 3;
-        int st = 0;
-        int q;
-        int a=0;
-        int x1;
-        int x2;
-        int y1;
-        int y2;
-        List<Integer> numbers = new ArrayList<>();
-        Scanner in = new Scanner(System.in);
-        Random random = new Random();
-        number = random.nextInt(90000) + 10000;
-        numbers.add(2);
-        while (a==0) {
-            for (int i = 0; i < numbers.size(); i++) {
-                if (n % numbers.get(i) == 0) {
-                    break;
-                }
-                if (numbers.get(i) >= Math.sqrt(n)) {
-                    st++;
-                    numbers.add(n);
-                    break;
-                }
-            }
-            if (st > number-1) {
-                q = (n - 1) / 2;
-                if (numbers.contains(q)) {
-                    for (int i = 3; i < numbers.size(); i++) {
-                        a = numbers.get(random.nextInt(number - 2));
-                        if ((a ^ q) % numbers.get(st) != 1) {
-                            break;
-                        } else {
-                            a = 0;
-                        }
-                    }
-                }
-            }
-            n += 2;
-        }
-        System.out.println(a*numbers.get(st));
 
-        System.out.println("Число P =  " + numbers.get(st));
-        System.out.println("Число A =  " + a);
-        System.out.println("Алиса придумала число X1(X1<" + numbers.get(st) + ") : ");
-        x1 = in.nextInt();
-        System.out.println("Боб придумал число X2(X2<" + numbers.get(st) + ") : ");
-        x2 = in.nextInt();
-        y1 = (a^x1)%numbers.get(st);
-        y2 = (a^x2)%numbers.get(st);
-        System.out.println("Алиса отправила Бобу свой открытый ключ - " + y1);
-        System.out.println("Боб отправил Алисе свой открытый ключ - " + y2);
-        System.out.println("Алиса сгенерировала закрытый ключ - " + (y2^x1)%numbers.get(st));
-        System.out.println("Боб сгенерировал закрытый ключ - " + (y1^x2)%numbers.get(st));
-        if((y2^x1)%numbers.get(st)==(y1^x2)%numbers.get(st)){
-            System.out.println("Симметричный сенас шифрования установлен");
+public class Main {
+    private static BigInteger p;
+    private static BigInteger a;
+    public static void main(String[] args) {
+        p = BigInteger.probablePrime(48, new Random());
+        a = BigInteger.probablePrime(48, new Random());
+        while(p.longValue()<a.longValue()){
+            a = BigInteger.probablePrime(48, new Random());
         }
-        else{
-            System.out.println("Ошибка");
-        }
+        Deffi client = new Deffi(p, a);
+        Deffi server = new Deffi(p, a);
+        client.setZ1(server.getY1());
+        server.setZ1(client.getY1());
+        System.out.println("p - " + p);
+        System.out.println("a - " + a);
+        System.out.println("Клиент получил - " + client.getZ1());
+        System.out.println("Клиент получил  X1 - " + client.getX1());
+        System.out.println("Сервер получил - " + server.getZ1());
+        System.out.println("Сервер получил  X2 - " + server.getX1());
     }
+
 }
